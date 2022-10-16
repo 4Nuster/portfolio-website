a273b8fbe7bb79e225b8bbaf574a1c3479677c0e
Yassine Bacha <nfstopplayer@gmail.com>
2022-10-16 14:37:25 +0100

    created a separate certificate card component (it does not display images yet) also added a bunch of certificates

---
+++ src/components/CertificateCardComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card class="cert-card drop-shadow-[-.5rem_.5rem_4px_#1f3f6f49]">
+    <v-card-title>{{ link }}</v-card-title>
+    <v-img :src="link" :aspect-ratio="aspectRatio" cover />
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["link"],
+  data: () => ({
+    aspectRatio: 6 / 5,
+  }),
+};
+</script>
+
+<style scoped>
+.cert-card {
+  /* min-height: 300px; */
+  min-width: 450px;
+  border-radius: 5%;
+  animation: scroll-certs 25s linear alternate infinite;
+}
+
+@keyframes scroll-certs {
+  from {
+    left: 10%;
+  }
+  to {
+    left: -225%;
+  }
+}
+</style>
